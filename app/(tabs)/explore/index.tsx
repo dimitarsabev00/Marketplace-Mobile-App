273aaff3cfef98e78d8b2ceae3bdb31b341e0ebe
Dimitar Sabev <dimitarsabev00@gmail.com>
2025-01-07 00:46:06 +0200

    Fix Bug In Explore Tab Screen

--- app/(tabs)/explore/index.tsx
+++ app/(tabs)/explore/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "@/configs/firebase";
 import LatestItemList from "@/components/LatestItemList";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ExploreScreen() {
   const [productList, setproductList] = useState([]);
@@ -25,9 +26,11 @@ export default function ExploreScreen() {
   };
 
   return (
-    <ScrollView className="p-5 py-8 bg-white">
-      <Text className="text-[30px] font-bold">Explore More</Text>
-      <LatestItemList latestItemList={productList} />
-    </ScrollView>
+    <SafeAreaView className="p-5 py-8 bg-white flex-1">
+      <ScrollView>
+        <Text className="text-[30px] font-bold">Explore More</Text>
+        <LatestItemList latestItemList={productList} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
