3b7147f4bbba72df7a24199ab817330522ff0404
Dimitar Sabev <dimitarsabev00@gmail.com>
2025-01-06 10:12:26 +0200

    Added Design & Logic For AddPost Tab Screen

--- configs/firebase.ts
+++ configs/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +12,10 @@ const firebaseConfig = {
   projectId: "marketplace-mobile-app-f6d81",
   storageBucket: "marketplace-mobile-app-f6d81.firebasestorage.app",
   messagingSenderId: "878351793714",
-  appId: "1:878351793714:web:b730267abf71c4cc0a062b"
+  appId: "1:878351793714:web:b730267abf71c4cc0a062b",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage();
