7aabfdead1b7d9e82d1954b652d9cd22e9c80468
Dimitar Sabev <dimitarsabev00@gmail.com>
2025-01-07 11:08:42 +0200

    Added Design & Logic For Profile Tab Screen + Logout Functionality

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,11 +1,75 @@
-import { ScrollView, Text } from "react-native";
+import { View, Text, Image, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth, useUser } from "@clerk/clerk-expo";
+import diary from "../../../assets/images/diary.png";
+import logout from "../../../assets/images/logout.png";
+import search from "../../../assets/images/search.png";
+import { router } from "expo-router";
 
 export default function ProfileScreen() {
+  const { user } = useUser();
+  const { isLoaded, signOut } = useAuth();
+  const menuList = [
+    {
+      id: 1,
+      name: "My Products",
+      icon: diary,
+      path: "/profile/my-products",
+    },
+    {
+      id: 2,
+      name: "Explore",
+      icon: search,
+      path: "explore",
+    },
+
+    {
+      id: 3,
+      name: "Logout",
+      icon: logout,
+    },
+  ];
+
+  const onMenuPress = (item) => {
+    if (item.name == "Logout") {
+      signOut();
+      router.replace("/login");
+      return;
+    }
+    item?.path ? router.navigate(item.path) : null;
+  };
+
   return (
-    <SafeAreaView>
-      <Text>ProfileScreen</Text>
-    </SafeAreaView>
+    <View className="p-5 bg-white flex-1">
+      <View className="items-center mt-14">
+        <Image
+          source={{ uri: user?.imageUrl }}
+          className="w-[100px] h-[100px] rounded-full"
+        />
+        <Text className="font-bold text-[25px] mt-2">{user?.fullName}</Text>
+        <Text className=" text-[18px] mt-2 text-gray-500">
+          {user?.primaryEmailAddress?.emailAddress}
+        </Text>
+      </View>
+
+      <FlatList
+        data={menuList}
+        numColumns={3}
+        style={{ marginTop: 20 }}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            onPress={() => onMenuPress(item)}
+            className="flex-1 p-3 border-[1px] items-center
+           mx-2 mt-4
+          rounded-lg border-blue-400 bg-blue-50"
+          >
+            {item.icon && (
+              <Image source={item?.icon} className="w-[50px]  h-[50px]" />
+            )}
+            <Text className="text-[12px] mt-2 text-blue-700">{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
   );
 }
